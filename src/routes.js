5f956296cdadcc71faee945c103eed04d78cf7c0
marcoslourinho <marcospereira.gnr@gmail.com>
2018-11-01 17:01:39 -0300

    fix routes

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,21 @@ import Checkin from './pages/checkin';
 export default createDrawerNavigator({
     Login: {
         screen: Login,
-    },
-    Checkin: {
-        screen: Checkin,
-    }, navigationOptions: {
+        navigationOptions: {
             headerStyle: {
                 backgroundColor: "#142638" //header color
             },
             headerTintColor: "#FFF" //heade font color
         }
+    },
+    Checkin: {
+        screen: Checkin,
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#142638" //header color
+            },
+            headerTintColor: "#FFF" //heade font color
+        }
+    }
     });
   
